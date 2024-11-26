56bd5d361a53d78ae7c2aa634e92a43e9593132a
vmaksimov <viktor.maksimov@oblak.bg>
2024-11-26 14:05:19 +0200

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+/*
+* user created modules
+ */
+
+import { UsersModule } from 'src/users/users.module';
+
+@Module({
+  imports: [UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
